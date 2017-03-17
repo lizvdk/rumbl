5e7e1550b3547d99812b931ecd56697ef59a2793
Liz Vanderkloot <liz@lizvdk.com>
2017-03-17 14:18:18 -0400

    Bring back Ecto

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,23 +1,3 @@
 defmodule Rumbl.Repo do
-
-  @moduledoc """
-  In memory repository
-  """
-
-  def all(Rumbl.User) do
-    [%Rumbl.User{id: "1", name: "Ada Lovelace", username: "alovelace", password: "Algorithm1"},
-     %Rumbl.User{id: "2", name: "Grace Hopper", username: "ghopper", password: "COBOL"},
-     %Rumbl.User{id: "3", name: "Margaret Hamilton", username: "mhamilton", password: "Apollo11"}]
-  end
-  def all(_modue), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
+  use Ecto.Repo, otp_app: :rumbl
 end
