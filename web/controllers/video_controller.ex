90d144e1e96e2253b813f8a0c5dc7eb992daca13
Liz Vanderkloot <liz@lizvdk.com>
2017-03-26 17:19:05 -0400

    User has_many Videos
    Associate newly created videos with current_user

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -8,13 +8,19 @@ defmodule Rumbl.VideoController do
     render(conn, "index.html", videos: videos)
   end
 
-  def new(conn, _params) do
-    changeset = Video.changeset(%Video{})
+  def new(conn, _params, user) do
+    changeset =
+      user
+      |> build_assoc(:videos)
+      |> Video.changeset
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"video" => video_params}) do
-    changeset = Video.changeset(%Video{}, video_params)
+  def create(conn, %{"video" => video_params}, user) do
+    changeset =
+      user
+      |> build_assoc(:videos)
+      |> Video.changeset(video_params)
 
     case Repo.insert(changeset) do
       {:ok, _video} ->
@@ -61,5 +67,14 @@ defmodule Rumbl.VideoController do
     conn
     |> put_flash(:info, "Video deleted successfully.")
     |> redirect(to: video_path(conn, :index))
+  end
+
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn),
+          [conn, conn.params, conn.assigns.current_user])
+  end
+
+  defp user_videos(user) do
+    assoc(user, :videos)
   end
 end
